27c340ddc02b01d361845b2e0c666dca03ca04c4
Shawon Ashraf <shawon13@live.com>
2024-04-14 01:06:38 +0200

    base api

--- api.py
+++ api.py
@@ -1,42 +1,73 @@
-from fastapi import FastAPI
-from schemas.book import Book, BookWithId
+from fastapi import FastAPI, Depends, HTTPException
+
+from sqlalchemy.orm import Session
+
+
+from database import crud, schemas
+from database.db import SessionLocal
+
+from loguru import logger
+
 
 app = FastAPI()
 
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
 async def root():
-    return {
-        "status": "ok",
-        "message": "biblio api is running!"
-    }
+    return {"status": "ok", "message": "biblio api is running!"}
 
-@app.get("/books")
-async def get_books():
-    return {
-        "status": "ok",
-        "books": []
-    }
-    
-@app.get("/books/{book_id}")
-async def get_book(book_id: int):
-    return {
-        "book": {},
-    }
-    
-@app.post("/books/add")
-async def add_book(book: Book):
-    return {
-        "book": {},
-    }
-    
-@app.put("/books/update/{book_id}")
-async def update_book(book_id: int):
-    return {
-        "book": {},
-    }
-    
-@app.delete("/books/delete/{book_id}")
-async def delete_book(book_id: int):
-    return {
-        "book": {},
-    }
+
+@app.get("/books", response_model=list[schemas.Book])
+async def get_books(db: Session = Depends(get_db)):
+    try:
+        db_books = crud.get_books(db)
+        return db_books
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        raise HTTPException(status_code=404, detail="Books not found!")
+
+
+@app.get("/books/{book_id}", response_model=schemas.Book)
+async def get_book(book_id: int, db: Session = Depends(get_db)):
+    try:
+        book = crud.get_book_by_id(db, book_id)
+        return book
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        raise HTTPException(status_code=404, detail="Book not found!")
+
+
+@app.post("/books/add", response_model=schemas.Book)
+async def add_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
+    new_book = crud.add_book(db, book)
+    if new_book:
+        logger.error("Book already exists!")
+        raise HTTPException(status_code=201, detail="Book already exists!")
+    return new_book
+
+
+@app.put("/books/update/{book_id}", response_model=schemas.Book)
+async def update_book(book_id: int, db: Session = Depends(get_db)):
+    book = crud.get_book_by_id(db, book_id)
+    if not book:
+        logger.error("Book not found!")
+        raise HTTPException(status_code=404, detail="Book not found!")
+    return crud.update_book(db, book_id, book)
+
+
+@app.delete("/books/delete/{book_id}", response_model=schemas.Book)
+async def delete_book(book_id: int, db: Session = Depends(get_db)):
+    book = crud.get_book_by_id(db, book_id)
+    if not book:
+        logger.error("Book not found!")
+        raise HTTPException(status_code=404, detail="Book not found!")
+    return crud.delete_book(db, book_id)
